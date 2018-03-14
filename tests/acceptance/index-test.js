331dac97b09df8f2bc57e543db9eaa8be6a0271c
AMS777 <alberto@oscuro.es>
2018-03-14 18:29:43 +0100

    Set page navbar with logo.

--- tests/acceptance/index-test.js
+++ tests/acceptance/index-test.js
@@ -5,18 +5,33 @@ import { setupApplicationTest } from 'ember-qunit';
 module('Acceptance | index', function(hooks) {
   setupApplicationTest(hooks);
 
-  test('visiting /', async function(assert) {
-    assert.expect(6);
+  test('Visit homepage.', async function(assert) {
+    assert.expect(2);
 
     await visit('/');
 
     assert.equal(currentURL(), '/');
 
     assert.notOk(find('#ember-welcome-page-id-selector'), 'Ember.js welcome page removed.');
-    
+  });
+
+  test('Check header-body-footer page layout.', async function(assert) {
+    assert.expect(4);
+
+    await visit('/');
+
     assert.ok(find('#app-layout'), 'App layout exists.');
     assert.ok(find('#app-layout > header'), 'Header exists in app layout.');
     assert.ok(find('#app-layout > main'), 'Body exists in app layout.');
     assert.ok(find('#app-layout > footer'), 'Footer exists in app layout.');
+  });
+
+  test('Check page navbar.', async function(assert) {
+    assert.expect(2);
+
+    await visit('/');
+
+    assert.ok(find('#app-layout > header > md-toolbar'), 'Navbar exists on page header.');
+    assert.ok(find('#app-layout > header > md-toolbar .logo'), 'Logo exists on page navbar.');
   });
 });
