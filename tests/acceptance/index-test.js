d1ba233b6ba57eb0bbbb42c2c7cb1c209907b2fd
AMS777 <alberto@oscuro.es>
2018-03-14 14:45:55 +0100

    Remove Ember.js welcome page.

--- tests/acceptance/index-test.js
+++ tests/acceptance/index-test.js
@@ -1,13 +1,16 @@
 import { module, test } from 'qunit';
-import { visit, currentURL } from '@ember/test-helpers';
+import { visit, currentURL, find } from '@ember/test-helpers';
 import { setupApplicationTest } from 'ember-qunit';
 
 module('Acceptance | index', function(hooks) {
   setupApplicationTest(hooks);
 
   test('visiting /', async function(assert) {
+    assert.expect(2);
+
     await visit('/');
 
     assert.equal(currentURL(), '/');
+    assert.notOk(find('#ember-welcome-page-id-selector'), 'Ember.js welcome page removed.');
   });
 });
