37bdbe6669e12be466d82362ec39f063df0abd56
AMS777 <alberto@oscuro.es>
2018-03-14 17:03:36 +0100

    Set header-body-footer page layout.

--- tests/acceptance/index-test.js
+++ tests/acceptance/index-test.js
@@ -6,11 +6,17 @@ module('Acceptance | index', function(hooks) {
   setupApplicationTest(hooks);
 
   test('visiting /', async function(assert) {
-    assert.expect(2);
+    assert.expect(6);
 
     await visit('/');
 
     assert.equal(currentURL(), '/');
+
     assert.notOk(find('#ember-welcome-page-id-selector'), 'Ember.js welcome page removed.');
+    
+    assert.ok(find('#app-layout'), 'App layout exists.');
+    assert.ok(find('#app-layout > header'), 'Header exists in app layout.');
+    assert.ok(find('#app-layout > main'), 'Body exists in app layout.');
+    assert.ok(find('#app-layout > footer'), 'Footer exists in app layout.');
   });
 });
