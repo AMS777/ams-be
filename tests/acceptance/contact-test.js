03319dc80a8795ae38feeb11808a9def7447ee12
AMS777 <alberto@oscuro.es>
2018-03-16 21:25:57 +0100

    Contact form validation tests.

--- tests/acceptance/contact-test.js
+++ tests/acceptance/contact-test.js
@@ -1,5 +1,5 @@
 import { module, test, skip } from 'qunit';
-import { visit, currentURL, find } from '@ember/test-helpers';
+import { visit, currentURL, find, click, fillIn } from '@ember/test-helpers';
 import { setupApplicationTest } from 'ember-qunit';
 
 module('Acceptance | contact', function(hooks) {
@@ -25,15 +25,35 @@ module('Acceptance | contact', function(hooks) {
     assert.ok(find(tsContactForm + '[data-test-submit]'), 'Contact form has submit button.');
   });
 
-  skip('Validate contact form', async function(assert) {
+  test('Validate contact form', async function(assert) {
     await visit('/contact');
 
-    assert.notOk(true);
+    // "ts": "test selector"
+    const tsContactForm = '[data-test-contact-form] ';
+
+    const requiredMessage = 'This is required.';
+    await click(tsContactForm + '[data-test-submit]');
+    assert.equal(find(tsContactForm + '[data-test-name] .paper-input-error').textContent.trim(),
+      requiredMessage, 'Validate empty name.');
+    assert.equal(find(tsContactForm + '[data-test-email] .paper-input-error').textContent.trim(),
+      requiredMessage, 'Validate empty email.');
+    assert.equal(find(tsContactForm + '[data-test-message] .paper-input-error').textContent.trim(),
+      requiredMessage, 'Validate empty message.');
+
+    await fillIn(tsContactForm + '[data-test-email] input', 'invalid-email-format');
+    await click(tsContactForm + '[data-test-submit]');
+    assert.ok(find(tsContactForm + '[data-test-email].md-focused'), 'Validate valid email format.');
   });
 
   skip('Submit contact form', async function(assert) {
     await visit('/contact');
 
-    assert.notOk(true);
+    // "ts": "test selector"
+    const tsContactForm = '[data-test-contact-form] ';
+
+    await fillIn(tsContactForm + '[data-test-name] input', 'Test Name');
+    await fillIn(tsContactForm + '[data-test-email] input', 'valid@email.format');
+    await fillIn(tsContactForm + '[data-test-message] textarea', 'Test message.');
+    await click(tsContactForm + '[data-test-submit]');
   });
 });
