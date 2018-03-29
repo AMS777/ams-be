ab743064d772c91adb23477805a13ce57b6d4c65
AMS777 <alberto@oscuro.es>
2018-03-29 21:00:46 +0200

    Fix navbar logo tests.

--- tests/integration/components/page-navbar-test.js
+++ tests/integration/components/page-navbar-test.js
@@ -6,23 +6,12 @@ import hbs from 'htmlbars-inline-precompile';
 module('Integration | Component | page-navbar', function(hooks) {
   setupRenderingTest(hooks);
 
-  test('Component renders.', async function(assert) {
-    // Set any properties with this.set('myProperty', 'value');
-    // Handle any actions with this.set('myAction', function(val) { ... });
-
-    await render(hbs`{{page-navbar}}`);
-
-    assert.equal(this.element.textContent.trim(), '');
-  });
-
   test('Check navbar logo.', async function(assert) {
-    // Set any properties with this.set('myProperty', 'value');
-    // Handle any actions with this.set('myAction', function(val) { ... });
 
     await render(hbs`{{page-navbar}}`);
 
     const navbarLogoSelector = '[data-test-page-navbar] [data-test-page-navbar-logo] ';
-    assert.ok(find(navbarLogoSelector), 'Logo exists on page navbar.');
-    assert.ok(find(navbarLogoSelector + 'img'), 'Logo has an image inside.');
+    assert.dom(navbarLogoSelector).exists('Logo exists on page navbar.');
+    assert.dom(navbarLogoSelector + 'img').exists('Logo has an image inside.');
   });
 });
